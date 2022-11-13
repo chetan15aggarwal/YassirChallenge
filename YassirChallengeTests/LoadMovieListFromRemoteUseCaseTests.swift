ecb2ef230443cea53708fd4bf0937f476a8d79e5
Chetan Aggarwal <chetan15aggarwal@gmail.com>
2022-11-13 23:15:50 +0100

    Test load method should delivers error on client error

--- YassirChallengeTests/LoadMovieListFromRemoteUseCaseTests.swift
+++ YassirChallengeTests/LoadMovieListFromRemoteUseCaseTests.swift
@@ -32,6 +32,15 @@ final class LoadMovieListFromRemoteUseCaseTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        
+        expect(sut, toCompleteWith: failure(.connectivity) ) {
+            let completionError = NSError(domain: "Test", code: 0)
+            client.complete(with: completionError)
+        }
+    }
+    
     // MARK: - Helpers
     private func makeSUT(url: URL = URL(string: "http://a-given-url.com")!,
                          file: StaticString = #filePath,
@@ -54,6 +63,35 @@ final class LoadMovieListFromRemoteUseCaseTests: XCTestCase {
             XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak", file: file, line: line)
         }
     }
+    
+    private func expect(_ sut: RemoteMovieListLoader,
+                        toCompleteWith expectedResult: RemoteMovieListLoader.Result,
+                        file: StaticString = #filePath,
+                        line: UInt = #line,
+                        when action: () -> Void ) {
+        
+        let exp = expectation(description: "Wait for load comletion")
+        
+        sut.load { receivedResults in
+            switch (receivedResults, expectedResult) {
+            case let (.success(receivedItems), .success(expectedItems)):
+                XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
+            case let (.failure(receivedError as RemoteMovieListLoader.Error), .failure(expectedError as RemoteMovieListLoader.Error)):
+                XCTAssertEqual(receivedError, expectedError)
+            default:
+                XCTFail("expected result \(expectedResult) got \(receivedResults) instead", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        
+        action()
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    private func failure(_ error: RemoteMovieListLoader.Error) -> RemoteMovieListLoader.Result {
+        return .failure(error)
+    }
+    
     
     // MARK: - HTTPClientSpy
     private final class HTTPClientSpy: HTTPClient {
