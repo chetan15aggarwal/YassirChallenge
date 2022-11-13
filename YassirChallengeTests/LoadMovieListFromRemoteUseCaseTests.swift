288b32bb799eb8b8dc3d4f43629e962b5095b54a
Chetan Aggarwal <chetan15aggarwal@gmail.com>
2022-11-13 23:06:58 +0100

    Test initialiser does not request data from url

--- YassirChallengeTests/LoadMovieListFromRemoteUseCaseTests.swift
+++ YassirChallengeTests/LoadMovieListFromRemoteUseCaseTests.swift
@@ -4,8 +4,49 @@
 
 
 import XCTest
+@testable import YassirChallenge
 
 final class LoadMovieListFromRemoteUseCaseTests: XCTestCase {
 
-
+    func test_init_deosNotRequestDataFromURL() {
+        let (_, client) = makeSUT()
+        XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+    // MARK: - Helpers
+    private func makeSUT(url: URL = URL(string: "http://a-given-url.com")!,
+                         file: StaticString = #filePath,
+                         line: UInt = #line) -> (RemoteMovieListLoader, HTTPClientSpy) {
+        
+        let client = HTTPClientSpy()
+        let sut = RemoteMovieListLoader(url: url, client: client)
+        
+        return (sut, client)
+    }
+    
+    // MARK: - HTTPClientSpy
+    private final class HTTPClientSpy: HTTPClient {
+        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
+        
+        var requestedURLs: [URL] {
+            return messages.map { $0.url }
+        }
+        
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void ) {
+            messages.append((url, completion))
+        }
+        
+        func complete(with error: Error, index: Int = 0) {
+            messages[index].completion(.failure(error))
+        }
+        
+        func complete(withStatusCode code: Int, data: Data, index: Int = 0) {
+            let response = HTTPURLResponse(
+                url: requestedURLs[index],
+                statusCode: code,
+                httpVersion: nil,
+                headerFields: nil)!
+            messages[index].completion(.success(data, response))
+        }
+    }
 }
