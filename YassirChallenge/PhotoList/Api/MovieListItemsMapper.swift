00560672e39dd29a2fa47db4770c798de1e28647
Chetan Aggarwal <chetan15aggarwal@gmail.com>
2022-11-13 18:28:37 +0100

    `RemoteMovieListLoader` implementation added with `MovieListItemsMapper` handling

--- YassirChallenge/PhotoList/Api/MovieListItemsMapper.swift
+++ YassirChallenge/PhotoList/Api/MovieListItemsMapper.swift
@@ -4,3 +4,26 @@
 
 
 import Foundation
+
+internal final class MovieListItemsMapper {
+    
+    private static let OK_200 = 200
+    private struct Root: Decodable {
+        let page: UInt
+        let results: [MovieListItemDTO]
+        let totalPages: String
+        let totalResults: String
+    }
+    
+    internal static func map(_ data: Data, _ response: HTTPURLResponse) throws  -> [MovieListItemDTO] {
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        guard response.statusCode == OK_200,
+              let root = try? decoder.decode(Root.self, from: data) else {
+                  throw RemoteMovieListLoader.Error.invalidData
+        }
+        return root.results
+    }
+}
--- YassirChallenge/PhotoList/Api/RemoteMovieListLoader.swift
+++ YassirChallenge/PhotoList/Api/RemoteMovieListLoader.swift
@@ -4,3 +4,44 @@
 
 
 import Foundation
+
+final class RemoteMovieListLoader: MovieListLoader {
+    
+    typealias Result = LoadMovieListResult
+    
+    enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    // MARK: - Properties and Initialiser
+    private let url: URL
+    private let client: HTTPClient
+    
+    init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    // MARK: - VehicleLoader
+    func load(completion: @escaping (LoadMovieListResult) -> Void ) {
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            switch result {
+            case let .success(data, response):
+                completion(RemoteMovieListLoader.map(data, response))
+            case .failure:
+                completion(.failure(Error.connectivity))
+            }
+        }
+    }
+    
+    private static func map(_ data: Data, _ response: HTTPURLResponse) -> Result {
+        do {
+            let items = try MovieListItemsMapper.map(data, response)
+            return .success(items.toModels())
+        } catch {
+            return .failure(error)
+        }
+    }
+}
