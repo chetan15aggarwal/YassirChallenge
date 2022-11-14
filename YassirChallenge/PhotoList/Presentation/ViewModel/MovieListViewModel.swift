3e3f9ab870f10cc6c9f172e5ef6ab5ae84287e62
Chetan Aggarwal <chetan15aggarwal@gmail.com>
2022-11-14 10:26:53 +0100

    Declaring `MovieListViewModeling` and implementation of it in `MovieListViewModel`

--- YassirChallenge/PhotoList/Presentation/ViewModel/MovieListViewModel.swift
+++ YassirChallenge/PhotoList/Presentation/ViewModel/MovieListViewModel.swift
@@ -5,7 +5,51 @@
 
 import Foundation
 
+protocol MovieListViewModeling {
+    var errorMessage: Container<String?> { get }
+    var shouldReloadTableView: Container<Bool> { get }
+    
+    func fetchMovieList()
+    
+    func numberOfRows(in section: Int) -> Int
+    func data(for indexpath: IndexPath) -> MovieListItem
+    func didSelect(indexPath: IndexPath) -> MovieListItem
+}
 
-final class MovieListViewModel {
+final class MovieListViewModel: MovieListViewModeling {
+    let movieListLoader: MovieListLoader
+    var movieList: [MovieListItem] = []
+    
+    var errorMessage = Container<String?>(value: nil)
+    var shouldReloadTableView : Container<Bool> = Container.init(value: false)
+    
+    init(with _movieListLoader: MovieListLoader) {
+        self.movieListLoader = _movieListLoader
+    }
+    
+    func fetchMovieList() {
+        movieListLoader.load { result in
+            switch result {
+            case let .success(movieList):
+                self.movieList = movieList
+                self.shouldReloadTableView.value = true
+                
+            case let .failure(error):
+                self.errorMessage.value = error.localizedDescription
+            }
+        }
+    }
+    
+    func numberOfRows(in section: Int) -> Int {
+        return movieList.count
+    }
+    
+    func data(for indexpath: IndexPath) -> MovieListItem {
+        return movieList[indexpath.row]
+    }
+    
+    func didSelect(indexPath: IndexPath) -> MovieListItem {
+        return movieList[indexPath.row]
+    }
     
 }
