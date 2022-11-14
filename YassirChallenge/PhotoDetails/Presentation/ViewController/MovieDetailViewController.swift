aa3105c9bf3f6be526ccfe563a2be9118eae744a
Chetan Aggarwal <chetan15aggarwal@gmail.com>
2022-11-14 15:44:53 +0100

    Designed Movie Detail List view controller

--- YassirChallenge/PhotoDetails/Presentation/ViewController/MovieDetailViewController.swift
+++ YassirChallenge/PhotoDetails/Presentation/ViewController/MovieDetailViewController.swift
@@ -6,13 +6,15 @@
 import UIKit
 
 class MovieDetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var averageVoteLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
