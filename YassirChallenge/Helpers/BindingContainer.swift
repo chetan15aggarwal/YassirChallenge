1ef0b6564296fb603a45d5fe6d9655ee47554d6b
Chetan Aggarwal <chetan15aggarwal@gmail.com>
2022-11-14 10:25:58 +0100

    Add `BindingContainer` for binding values between view controller and view model

---
+++ YassirChallenge/Helpers/BindingContainer.swift
@@ -0,0 +1,28 @@
+//
+// YassirChallenge
+// Created by Chetan Aggarwal.
+
+
+import Foundation
+
+final class Container<Element> {
+    typealias CompletionHandler = (Element?) -> Void
+    
+    var handler: CompletionHandler?
+    var value: Element? {
+        didSet {
+            guard let value = self.value else {return}
+            handler?(value)
+        }
+    }
+    
+    init(value: Element?, handler: CompletionHandler? = nil) {
+        self.value = value
+        self.handler = handler
+    }
+    
+    func bind(withHandler handler: @escaping CompletionHandler) {
+        self.handler = handler
+        self.handler!(value)
+    }
+}
