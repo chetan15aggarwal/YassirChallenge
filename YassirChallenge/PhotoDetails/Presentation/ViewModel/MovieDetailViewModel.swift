e161d85ebccdeae87c7b291eb8adf34f21533b18
Chetan Aggarwal <chetan15aggarwal@gmail.com>
2022-11-14 16:20:30 +0100

    `MovieDetailViewModel` implemented with `MovieDetailViewModeling`

--- YassirChallenge/PhotoDetails/Presentation/ViewModel/MovieDetailViewModel.swift
+++ YassirChallenge/PhotoDetails/Presentation/ViewModel/MovieDetailViewModel.swift
@@ -4,3 +4,32 @@
 
 
 import Foundation
+
+protocol MovieDetailViewModeling {
+    var errorMessage: Container<String?> { get }
+    var shouldRefreshView: Container<MovieDetail?> { get }
+    
+    func fetchMovieDetails()
+}
+
+final class MovieDetailViewModel {
+    let movieDetailLoader: MovieDetailLoader
+    
+    var errorMessage = Container<String?>(value: nil)
+    var shouldReloadTableView : Container<MovieDetail?> = Container.init(value: nil)
+    
+    init(with _movieDetailLoader: MovieDetailLoader) {
+        self.movieDetailLoader = _movieDetailLoader
+    }
+    
+    func fetchMovieDetails(){
+        movieDetailLoader.load { result in
+            switch result {
+            case let .success(movieDetail):
+                self.shouldReloadTableView.value = movieDetail
+            case let .failure(error):
+                self.errorMessage.value = error.localizedDescription
+            }
+        }
+    }
+}
